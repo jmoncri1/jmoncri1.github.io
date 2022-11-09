6af72d27cb8ef00a1b821227298e26b66fc29d67
jmoncri1 <113453183+jmoncri1@users.noreply.github.com>
2022-11-09 08:34:15 -0500

    lms

--- assignments/lms-assignment/lms-assignment.js
+++ assignments/lms-assignment/lms-assignment.js
@@ -90,29 +90,72 @@ const testData = [
     submissionDate: "2/20/2020"
   }
 ];
-
-
+/*  make a function that will take a date and return the matching objects  */
+//name the function
 function onDateButtonClick() {
+  //make a variable that will hold the date input
   const dateInput = document.getElementById("dateInput");
+  //make a variable that will house what is displayed
   const submittedDisplay = document.getElementById('submittedDisplay');
-
+  const unsubmittedDisplay = document.getElementById("unsubmittedDisplay");
+  //this line helps to clear the display so that the user will not be able to add duplicate inputs
   submittedDisplay.innerHTML = "";
+  unsubmittedDisplay.innerHTML = "";
+  //make a variable that will hold the array of those submitted on a certain day
   let displaySubmittedArray = [];
+  //make a variable that will hold the array of those unsubmitted on a certain day
   let displayUnsubmittedArray = [];
+  //search the array (testData) through each item
   for (const item of testData) {
+    //check if the date input matches the date of the object
     if (item.submissionDate === dateInput.value) {
-      displaySubmittedArray.push(item)
-    } else {
-      displayUnsubmittedArray.push(item)
+      //if the dates match, add that object to the variable made for submitted
+      displaySubmittedArray.push(item);
     }
   }
-  console.log(displaySubmittedArray)
+
+  const studentNames = [];
+  for (const item of displaySubmittedArray) {
+    if (!studentNames.includes(item.studentName)) {
+      studentNames.push(item.studentName)
+    }
+  }
+  for (const item of testData) {
+    if (!studentNames.includes(item.studentName) && !displayUnsubmittedArray.includes(item.studentName)) {
+      displayUnsubmittedArray.push(item.studentName)
+    }
+  }
+
+
+
+
+
+
+  /*
+  //go through the array again
+  for (item of testData) {
+    //check if the date input matches the date of the object
+    if(item.submissionDate === dateInput.value) {
+     //if the date matches, remove it from the array
+     
+     //display just the names of the objects left
+    }
+  }*/
+
+
+
+
+
+
+  console.log("displaySubmittedArray: ", displaySubmittedArray);
+  console.log("displayUnsubmittedArray: ", displayUnsubmittedArray);
   for (const item of displaySubmittedArray) {
     submittedDisplay.innerHTML += `Student name: ${item.studentName}<br>Quiz Name: ${item.quizName}<br>Quiz Module: ${item.quizModule}<br>Quiz Score: ${item.quizScore}<br>Student ID: ${item.studentId}<br>Submission Date: ${item.submissionDate}<hr><br><br>`
-    document.getElementById("headingForUnsubmitted").innerText = `These are the students who have not submitted quizes on that date:`
   }
+  document.getElementById("headingForUnsubmitted").innerText = `These are the students who have not submitted quizes on that date:`
+
   for (const item of displayUnsubmittedArray) {
-    unsubmittedDisplay.innerHTML += `Student name: ${item.studentName}<br>Quiz Name: ${item.quizName}<br>Quiz Module: ${item.quizModule}<br>Quiz Score: ${item.quizScore}<br>Student ID: ${item.studentId}<br>Submission Date: ${item.submissionDate}<hr><br><br>`
+    unsubmittedDisplay.innerHTML += `Student name: ${item}`;
   }
 };
 
@@ -141,7 +184,7 @@ function onAverageButton() {
   let average = (sum / testData.length).toFixed(1);
   console.log(average);
 
-document.getElementById('displayAverage').innerText = `The average is: ${average}`;
+  document.getElementById('displayAverage').innerText = `The average is: ${average}`;
 
 }
 
